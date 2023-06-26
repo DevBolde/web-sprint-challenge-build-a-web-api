8b6fc8ac673dbc07543927cc8b339c94caf0ae48
Devin Bolden <devinbolden6@gmail.com>
2023-06-26 02:25:53 -0500

    finished projects first 2 "gets"

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,50 @@
 // Write your "projects" router here!
+const express = require('express')
+const Project = require('./projects-model')
+const router = express.Router()
+
+const {errorHandling} = require('./projects-middleware')
+
+
+router.get('/api/projects', errorHandling, (req, res, next) => {
+    Project.get()
+      .then(proj => {
+        res.json(proj)
+      })
+      .catch(error => {
+        // log error to server
+        next(error);
+      });
+});
+
+router.get('/api/projects/:id', errorHandling, async (req, res, next) => {
+    try {
+      const { id } = req.params;
+  
+      if (!id) {
+        return res.status(404).json({
+          message: `Project ID is missing or invalid`,
+        });
+      }
+  
+      Project.get(id)
+        .then(proj => {
+          if (!proj) {
+            return res.status(404).json({
+              message: `Project with ID ${id} cannot be found`,
+            });
+          }
+  
+          res.json(proj);
+        })
+        .catch(error => {
+          next(error);
+        });
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  router
+
+  module.exports = router;
