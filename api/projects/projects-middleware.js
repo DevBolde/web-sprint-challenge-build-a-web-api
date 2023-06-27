f6a4e1ec491bde8af90978bcf1e0d1d9c9a6bb31
Devin Bolden <devinbolden6@gmail.com>
2023-06-26 21:04:13 -0500

    finished post and most of put for projects

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,44 @@
 // add middlewares here related to projects
+const Project = require('./projects-model')
+
+function errorHandling(err, req, res, next) { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: `Horror in the router: ${err.message}`,
+    stack: err.stack,
+  });
+}
+function validateId(req, res, next) {
+  const { id } = req.params;
+  Hubs.findById(id)
+    .then(hub => {
+      if (hub) {
+        req.hub = hub;
+        next();
+      } else {
+        res.status(404).json({ message: "Invalid id; hub not found" });
+
+        // error handling middleware option:
+        // next({ message: "Invalid id; hub not found"});
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error processing request',
+      });
+    });
+}
+function requiredBody(req, res, next) {
+  if (req.body && Object.keys(req.body).length > 0) {
+    next();
+  } else {
+    res.status(400).json({ message: "Please include request body" });
+
+    // error handling middleware option:
+    // next({ message: "Please include request body" }));
+  }
+}
+  module.exports = {
+    errorHandling,
+  }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -45,6 +45,53 @@ router.get('/api/projects/:id', errorHandling, async (req, res, next) => {
     }
   });
   
-  router
+router.post('/api/projects', errorHandling,  async (req, res, next) => {
+    try {
+      
+      const project = req.body; // Assuming the project data is passed in the request body
+  
+      // Insert the project into the database
+      if(!project.name || !project.description){
+       return  res.status(400).json({
+          message: "Error: you need name AND description"
+        })
+      }
+      const insertedProject = await Project.insert(project);
 
+         res.status(201).json(insertedProject);
+
+    } catch (error) {
+      next(error)
+    }
+  });
+router.put('/api/projects/:id', errorHandling, async (req, res, next) => {
+  try{
+    const {id} = req.params;
+    const {name, description, completed} = req.body;
+    
+    if (!name && !description && !completed) {
+      return res.status(400).json({
+        message: `project with ${id} ID does not exist!`,
+      });
+    }else{
+        const updateProject = await Project.update(id, {name, description, completed})
+      .then(proj => {
+        if(!proj){
+          res.status(404).json({
+            message: 'the project could not be found'
+          })
+        }else{
+          console.log(proj)
+           res.status(200).json(proj)
+        }
+      })
+      .catch(error => {
+        next(error);
+      })
+    }
+  }catch(error){
+    next(error)
+  }
+ 
+})
   module.exports = router;
